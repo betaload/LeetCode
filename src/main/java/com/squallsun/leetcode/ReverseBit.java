8b6cd8b65a24229db2ff5f38b47b22c4c1dd9a64
squallsun <squallsun@hotmail.com>
2015-03-01 17:35:56 +0800

    reverse int

---
+++ src/main/java/com/squallsun/leetcode/ReverseBit.java
@@ -0,0 +1,33 @@
+package com.squallsun.leetcode;
+
+/**
+ * Created by SJQ on 2015/3/1.
+ */
+public class ReverseBit {
+
+    public static void reverse(int x) {
+
+        int size = Integer.SIZE;
+        for (int i = 0; i< size/2; i++) {
+            int j = size - i - 1;
+
+            int low = (x>>i) & 1;
+            int high = (x>>j) & 1;
+
+            if((low ^ high) == 1) {
+                x = x ^ ((1<<i) | (1 <<j));
+            }
+
+        }
+
+        System.out.println(x);
+
+    }
+
+    public static void main(String[] args) {
+
+        reverse(1000000);
+
+
+    }
+}
